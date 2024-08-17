7218d7196df9f76e891e5ed97ff31a7600fb2637
GITHUB <samimmanvelj@gmail.com>
2024-08-17 12:06:36 +0530

    initialcommit

---
+++ src/test/java/sam/gitclass.java
@@ -0,0 +1,17 @@
+package sam;
+
+public class gitclass {
+
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+}
